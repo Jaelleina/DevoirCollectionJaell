66c1132d7fad8e94d28c2cc4eb87daa8f130ee49
inanganjijaelle@gmail.com <inanganjijaelle@gmail.com>
2023-05-12 11:11:50 +0200

    #3.cration d' une listse des elements

--- main.py
+++ main.py
@@ -21,3 +21,16 @@ print("2.--------changement du contenu de l'element no5----------\n")
 listes[4]='Forte'
 
 print(listes,"\n\n")
+
+
+#3.cration d' une listse des elements contenant la  lettre "a"
+
+print("3.-------creation d'une list des element contenant la lettre 'a'--------\ n")
+
+liste_copy=[]  
+for li in listes:
+    if'a' in li:
+         
+         liste_copy.append(li)
+
+print(liste_copy,"\n\n")
