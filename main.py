3fcf86e412dcf6ec4a25225ce96cb2989b0e94dc
inanganjijaelle@gmail.com <inanganjijaelle@gmail.com>
2023-05-12 11:10:02 +0200

    #1. List of strings

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # -------question 1
 
+#1. List of strings 
+#1. creation de la liste
 
+print("1.-----------liste de 10 elements de type chaine de caracteur------\n")
+
+
+
+listes=['Billy','Jeanne','Titi','Milly','Jacky','Marie','Ginette','Moses','Lily','Siella']
+
+
+
+print(listes,"\n\n")
